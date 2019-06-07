18c6965ddba280fa2fee84691858d0d52304c101
Guillaume130476 <guillaume.morillon@gmx.fr>
2019-06-07 19:01:27 +0200

    ajout du fichier view template

---
+++ view/template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr" xml:lang="fr">
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content= />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  <link rel="stylesheet" href="http://localhost/blogfr/css/style.css">
+  <link rel="stylesheet" href="http://localhost/blogfr/css/styles.css">
+   <link rel="stylesheet" href="http://localhost/blogfr/css/tiny.css">
+   
+   
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=u682ajjtchblk3l9wwujfnabzjwrum4doif77io9a82nmwmi"></script>
+  <script>tinymce.init({forced_root_block : false,selector: '#mytextarea'});
+  </script>
+  <title>Blog</title>
+  <link rel="icon" href="images/favicon2.ico" />
+  
+</head>
+        
+    <body>
+        <?= $content ?>
+
+      <!-- liens pour les fichiers javascript-->
+  <script src="http://localhost/blogfr/js/jquery-3.3.1.js"></script>
+  <script src="http://localhost/blogfr/js/diaporama.js"></script>
+  <script src="http://localhost/blogfr/js/scripts.js"></script> 
+  <script src="http://localhost/blogfr/js/jsRedactor.js"></script>
+  <script src="http://localhost/blogfr/js/jsContact.js"></script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>  
+    </body>
+</html>
