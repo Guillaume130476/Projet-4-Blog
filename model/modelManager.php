0efb9d83d58f9ca7d9db608e5e10b03c97569c84
Guillaume130476 <guillaume.morillon@gmx.fr>
2019-06-07 18:41:05 +0200

    ajout du modelmanager pour gestion en poo de la connexion à la BDD

---
+++ model/modelManager.php
@@ -0,0 +1,20 @@
+<?php
+
+class modelManager
+{
+
+	protected  function dbConnect()
+{
+	    try
+	{$db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+	   
+	    return $db;
+
+	}
+	catch(Exception $e)
+	{
+	    die('Erreur : '.$e->getMessage());
+	}
+}
+     
+}
