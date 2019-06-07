7f7e51192a828aa9ce720dcfce00b1368f959cf7
Guillaume130476 <guillaume.morillon@gmx.fr>
2019-06-07 18:54:43 +0200

    ajout du dossier view et fichiers header et footer mvc

---
+++ view/footer.php
@@ -0,0 +1,29 @@
+
+<body data-spy="scroll" data-target=".navbar">
+  <div class="container-fluid">
+    <div class="row">
+      <footer class="col-xs-12">
+        <div id="reseau1" class="col-xs-offset-9 col-xs-1">
+          <a href="">
+            <i class="fab fa-twitter-square fa-2x"></i>
+          </a>
+        </div>
+        <div  id="reseau2" class="col-xs-1">
+          <a href="">
+            <i class="fab fa-facebook fa-2x"></i>
+          </a>
+        </div> 
+        <div id="reseau3" class="col-xs-1">
+          <a href="">
+            <i class="fas fa-rss fa-2x"></i>
+          </a>
+        </div>
+      </footer>
+    </div>
+  </div>
+  <!-- jQuery -->
+  <script src="../js/jquery-3.3.1.js"></script>
+  <!-- Javascript de Bootstrap -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+
---
+++ view/header.php
@@ -0,0 +1,39 @@
+
+
+<body data-spy="scroll" data-target=".navbar">
+
+  <header>
+
+    <!-- Navigation
+    ================================================== -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="http://localhost/blogfr/index.html">Jean Forteroche</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li>
+           <a href="http://localhost/blogfr/index.php">Accueil</a>
+            </li>
+          <li>
+           <a href="http://localhost/blogfr/view/frontend/auteur.php">L'Auteur</a> 
+         </li>
+          <li>
+           <a href="http://localhost/blogfr/view/frontend/login.php">Administration</a>
+            </li>
+          <li>
+           <a href="http://localhost/blogfr/view/frontend/contact.php">Contact</a>
+            </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+
+</body>
+
