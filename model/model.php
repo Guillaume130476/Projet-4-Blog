b27f423fb4d72fb945de12395e0a09d5a584c1d5
Guillaume130476 <guillaume.morillon@gmx.fr>
2019-06-07 18:01:18 +0200

    ajout des models front et backend mvc

---
+++ model/model.php
@@ -0,0 +1,118 @@
+<?php
+
+require_once("model/modelManager.php");
+
+class FrontManager extends modelManager
+{
+
+
+public function getPosts()
+{
+
+$db = $this->dbConnect();
+$req = $db->query( 'SELECT  *   FROM posts');
+
+return $req;
+
+
+}
+
+
+
+
+
+public function getPost($postId)
+{
+
+$db = $this->dbConnect();
+$req = $db->prepare('SELECT id, Title, Content, date_publi FROM posts WHERE id = ?');
+$req->execute(array($postId));
+$post = $req->fetch();
+
+return $post;
+
+
+}
+
+
+
+public function getComments($postId)
+{
+
+
+$db = $this->dbConnect();
+$comments = $db->prepare('SELECT id, id_billet, author, comment, date_comment FROM comments WHERE id_billet = ? ORDER BY date_comment');
+$comments->execute(array($postId));
+return $comments;
+
+
+}
+
+
+
+public function postComment($postId, $author, $comment)
+{
+    $db = $this->dbConnect();
+    $comments = $db->prepare('INSERT INTO comments_buffer (id_billet, author, comment, date_comment, signalement) VALUES(?, ?, ?, NOW(),0)');
+    $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+    return $affectedLines;
+}
+
+
+
+ public function envoiMailing ($nom, $prenom,$textarea, $adresseMail)
+{
+    $to  = 'guillaume.morillon@gmx.fr';
+    $nom = $nom;
+    $prenom = $prenom;
+    $message = $textarea;
+     
+
+     mail($to, $nom, $prenom, $message);
+     Echo 'mail a été envoyé';
+    
+}
+
+
+public function loginAdmin($login, $pass)
+{
+	$db = $this->dbConnect();
+	//  Récupération de l'utilisateur et de son pass hashé
+$req = $db->prepare('SELECT id, pass, prename, name  FROM admin WHERE login = :login');
+$req->execute(array(
+    'login' => $login));
+$resultat = $req->fetch();
+
+
+// Comparaison du pass envoyé via le formulaire avec la base
+$isPasswordCorrect = password_verify($pass, $resultat['pass']);
+
+if (!$resultat)
+{
+    echo 'Mauvais identifiant ou mot de passe !';
+}
+else
+{
+    if ($isPasswordCorrect) {
+        session_start();
+
+        $_SESSION['id'] = $resultat['id'];
+        $_SESSION['login'] = $_POST['login'];
+        $_SESSION['nom'] = $resultat['name'];
+        $_SESSION['prenom'] = $resultat['prename'];
+        header('Location: view/indexBackEnd.php');
+        exit();
+    }
+    else {
+        echo 'Mauvais identifiant ou mot de passe !';
+    }
+}
+
+
+}
+
+
+}
+
+?>
---
+++ model/modelBackEnd.php
@@ -0,0 +1,274 @@
+<?php
+
+require_once("modelManager.php");
+
+
+class BackManager extends modelManager
+{
+
+ public function getPosts()
+{
+
+$db = $this->dbConnect();
+$req = $db->query( 'SELECT  *   FROM posts');( 'SELECT  *   FROM comments_buffer');
+
+return $req;
+
+
+}
+
+
+
+
+
+ public function getPost($postId)
+{
+
+$db = $this->dbConnect();
+$req = $db->prepare('SELECT id, Title, Content, date_publi FROM posts WHERE id = ?');
+$req->execute(array($postId));
+$post = $req->fetch();
+
+return $post;
+
+
+}
+
+
+
+public function getComments($postId)
+{
+
+
+$db = $this->dbConnect();
+$comments = $db->prepare('SELECT id, id_billet, author, comment, date_comment FROM comments WHERE id_billet = ? ORDER BY date_comment');
+$comments->execute(array($postId));
+return $comments;
+
+
+}
+
+public function postComment($postId, $author, $comment)
+{
+    $db = $this->dbConnect();
+    $comments = $db->prepare('INSERT INTO comments (id_billet, author, comment, date_comment) VALUES(?, ?, ?, NOW())');
+    $affectedLines = $comments->execute(array($postId, $author, $comment));
+
+    return $affectedLines;
+}
+
+
+
+public function postPost($Title, $Content, $date_publi, $id_author)
+
+{
+
+   $db = $this->dbConnect();
+   // Insertion du message à l'aide d'une requête préparée
+$posts = $db->prepare('INSERT INTO posts (Title, Content, date_publi, id_author) VALUES( ?,?,?,?)');
+$affectedLines = $posts->execute(array($Title, $Content, $date_publi, $id_author ));
+
+return $affectedLines;
+    
+}
+
+
+public function updatePost ($Title, $Content, $date_publi, $id_author, $id )
+
+{
+
+   $db = $this->dbConnect();
+   // modification du message à l'aide d'une requête préparée
+$req = $db->prepare('UPDATE posts SET Title = :nvtitle, Content = :nvcontent, date_publi = :nvdate_publi, id_author = :nvid_author WHERE id = :id ');
+$req->execute(array(
+	'nvtitle'=> $Title,
+	'nvcontent'=> $Content,
+	'nvdate_publi'=> $date_publi,
+	'nvid_author'=> $id_author,
+	'id'=> $id
+	));
+
+return $req;
+
+    
+}
+
+public function deletePost($postId)
+
+{
+$db = $this->dbConnect();
+	
+$req = $db->prepare('DELETE FROM  posts WHERE id = ?');
+$req->execute(array($postId));
+$post = $req->fetch();
+
+return $post;
+
+
+}
+
+public function deleteComment($id)
+
+{
+	
+$db = $this->dbConnect();
+$req = $db->prepare('DELETE FROM  comments WHERE id = ?');
+$req->execute(array($id));
+$post = $req->fetch();
+
+return $post;
+
+
+}
+
+
+
+
+public function getavComments()
+{
+
+$db = $this->dbConnect();
+$req = $db->query( 'SELECT  *   FROM comments_buffer');
+
+return $req;
+
+
+}
+
+
+
+public function deleteBufferComment($id)
+
+{
+	
+$db = $this->dbConnect();
+$req = $db->prepare('DELETE FROM  comments_buffer WHERE id = ?');
+$req->execute(array($id));
+$post = $req->fetch();
+
+return $post;
+
+
+}
+
+public function validateBufferComment($id)
+
+{
+	
+$db = $this->dbConnect();
+$req = $db->prepare('INSERT INTO  comments (id,id_billet, author, comment, date_comment, signalement) SELECT id, id_billet, author, comment, date_comment, signalement FROM comments_buffer WHERE id = ?');
+$req->execute(array($id));
+$post = $req->fetch();
+return $post;
+}
+
+
+
+public function MessageValidateBufferComment()
+{
+	echo "<script>alert(\"la variable est nulle\")</script>";
+	
+}
+
+
+
+
+
+
+public function validateSignalComment($id)
+
+{
+  $db = $this->dbConnect();
+   // modification du message à l'aide d'une requête préparée
+$req = $db->prepare('UPDATE comments SET signalement = :nvsignalement WHERE id = :id ');
+$req->execute(array(
+	'nvsignalement'=> 1,
+	'id'=> $id
+	));
+
+return $req;
+
+
+}
+
+public function DisplayReportsComment()
+{
+
+$db = $this->dbConnect();
+$req = $db->query( 'SELECT  *   FROM comments WHERE signalement=1');
+
+return $req;
+
+
+}
+
+public function deleteSignalComment($id)
+
+{
+	
+$db = $this->dbConnect();
+$req = $db->prepare('DELETE FROM  comments WHERE id = ?');
+$req->execute(array($id));
+$post = $req->fetch();
+
+return $post;
+
+
+
+}
+
+public function cancelSignalComment($id)
+
+{
+	
+  $db = $this->dbConnect();
+   // modification du message à l'aide d'une requête préparée
+$req = $db->prepare('UPDATE comments SET signalement = :nvsignalement WHERE id = :id ');
+$req->execute(array(
+	'nvsignalement'=> 0,
+	'id'=> $id
+	));
+
+return $req;
+
+
+}
+
+public function disconnectSession()
+
+{
+	session_start();
+	session_unset ();
+	session_destroy();
+	 ?>
+    <script language="javascript">document.location="../index.php"</script>;<?php
+}
+
+
+//public function verificationSession()
+//{
+
+	//if (isset($_SESSION['nom']) && isset($_SESSION['prenom'])) {
+
+	// On teste pour voir si nos variables ont bien été enregistrées
+	//echo '<html>';
+	//echo '<head>';
+	//echo '<title>Page de notre section membre</title>';
+	//echo '</head>';
+
+	//echo '<body>';
+	//echo 'Votre login est '.$_SESSION['login'].' et votre mot de passe est '.$_SESSION['pwd'].'.';
+	//echo '<br />';
+
+	
+//}
+//else {
+	//
+    //<script language="javascript">document.location="../index.php"</script>;<?php
+//}
+//}
+
+
+}
+
+?>
