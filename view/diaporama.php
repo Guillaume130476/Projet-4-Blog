744aed5be0673cf91e97ea0f20e027494029415a
Guillaume130476 <guillaume.morillon@gmx.fr>
2019-06-13 09:05:43 +0200

    ajout de la view panorama

---
+++ view/diaporama.php
@@ -0,0 +1,24 @@
+
+
+<section id="contenantdiapo">
+		<!--contenant diaporama --> 
+	<div id="titrelivre">
+		<div id="boutons">
+			<div class="prev">
+				<i class="fas fa-angle-left fa-2x"></i>
+			</div>
+			<div class="next">
+				<i class="fas fa-angle-right fa-2x"></i>
+			</div>
+		</div>
+		<div id="billet">
+			<img class="images" src="http://localhost/blogfr/images/titrelivre.png" alt="paysage Alaska">
+		</div>
+	</div>
+	<div id="diaporama">
+		<img class="images" src="http://localhost/blogfr/images/canada1.jpg" alt="paysage Alaska">
+		<img class="images" src="http://localhost/blogfr/images/canada2.jpg" alt="paysage Alaska" >
+		<img class="images" src="http://localhost/blogfr/images/canada3.jpg" alt="paysage Alaska" >
+		<img class="images" src="http://localhost/blogfr/images/canada4.jpg" alt="paysage Alaska" >
+	</div>
+</section>
