3ae714490ffd973e6a63d1e30587a28f88fd9e9b
Guillaume130476 <guillaume.morillon@gmx.fr>
2019-06-16 17:32:20 +0200

    mise à jour du template avec intégration du header et footer

--- view/template.php
+++ view/template.php
@@ -1,34 +1,41 @@
 <!DOCTYPE html>
 <html lang="fr" xml:lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <meta name="description" content= />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-  <link rel="stylesheet" href="http://localhost/blogfr/css/style.css">
-  <link rel="stylesheet" href="http://localhost/blogfr/css/styles.css">
-   <link rel="stylesheet" href="http://localhost/blogfr/css/tiny.css">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Bienvenue sur le site du nouveau livre de Jean Forteroche,Billet simple pour l'Alaska ">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" href="http://localhost/blogfr/css/style.css">
+    <link rel="stylesheet" href="http://localhost/blogfr/css/styles.css">
+    <link rel="stylesheet" href="http://localhost/blogfr/css/tiny.css">
    
    
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=u682ajjtchblk3l9wwujfnabzjwrum4doif77io9a82nmwmi"></script>
-  <script>tinymce.init({forced_root_block : false,selector: '#mytextarea'});
-  </script>
-  <title>Blog</title>
-  <link rel="icon" href="images/favicon2.ico" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <script type="text/javascript" src="http://localhost/blogfr/js/tinymce/js/tinymce/tinymce.min.js"></script>
+    <script>
+      tinymce.init({
+      selector: '#mytextarea',  // change this value according to your HTML
+      language: 'fr_FR',
+      entity_encoding : "raw",
+      forced_root_block : "",
+      force_br_newlines : true,
+      force_p_newlines : false,
+      invalid_elements : 'p',
+       });
+    </script>
+    <title> Billet simple pour L'Alaska</title>
+    <link rel="icon" href="http://localhost/blogfr/images/favicon2.ico" />
   
-</head>
+  </head>
         
-    <body>
+  <body>
+    <?php require("http://localhost/blogfr/view/header.php"); ?>
         <?= $content ?>
-
+    <?php require("http://localhost/blogfr/view/footer.php"); ?>
       <!-- liens pour les fichiers javascript-->
-  <script src="http://localhost/blogfr/js/jquery-3.3.1.js"></script>
-  <script src="http://localhost/blogfr/js/diaporama.js"></script>
-  <script src="http://localhost/blogfr/js/scripts.js"></script> 
-  <script src="http://localhost/blogfr/js/jsRedactor.js"></script>
-  <script src="http://localhost/blogfr/js/jsContact.js"></script>
-  
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>  
-    </body>
+    <script src="http://localhost/blogfr/js/jquery-3.3.1.js"></script>
+    <script src="http://localhost/blogfr/js/diaporama.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> 
+  </body>
 </html>
