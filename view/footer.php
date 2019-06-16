38d37262f19cb95223e43af02fa76bcbec98d790
Guillaume130476 <guillaume.morillon@gmx.fr>
2019-06-16 17:33:49 +0200

    mise à jour du code du header et footer

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,9 @@
 
-<body data-spy="scroll" data-target=".navbar">
+<div data-spy="scroll" data-target=".navbar">
   <div class="container-fluid">
     <div class="row">
       <footer class="col-xs-12">
-        <div id="reseau1" class="col-xs-offset-9 col-xs-1">
+        <div id="reseau1" class="col-sm-offset-9 col-sm-1 col-xs-offset-8 col-xs-1">
           <a href="">
             <i class="fab fa-twitter-square fa-2x"></i>
           </a>
@@ -25,5 +25,5 @@
   <script src="../js/jquery-3.3.1.js"></script>
   <!-- Javascript de Bootstrap -->
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-</body>
+</div>
 
--- view/header.php
+++ view/header.php
@@ -1,11 +1,7 @@
 
 
-<body data-spy="scroll" data-target=".navbar">
-
+<div data-spy="scroll" data-target=".navbar">
   <header>
-
-    <!-- Navigation
-    ================================================== -->
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -18,22 +14,20 @@
       <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
           <li>
-           <a href="http://localhost/blogfr/index.php">Accueil</a>
-            </li>
+            <a href="http://localhost/blogfr/index.php">Accueil</a>
+          </li>
           <li>
-           <a href="http://localhost/blogfr/view/frontend/auteur.php">L'Auteur</a> 
-         </li>
+            <a href="http://localhost/blogfr/view/frontend/auteur.php">L'Auteur</a> 
+          </li>
           <li>
-           <a href="http://localhost/blogfr/view/frontend/login.php">Administration</a>
-            </li>
+            <a href="http://localhost/blogfr/view/frontend/login.php">Administration</a>
+          </li>
           <li>
-           <a href="http://localhost/blogfr/view/frontend/contact.php">Contact</a>
-            </li>
+            <a href="http://localhost/blogfr/view/frontend/contact.php">Contact</a>
+          </li>
         </ul>
       </div>
     </nav>
   </header>
-
-
-</body>
+</div>
 
